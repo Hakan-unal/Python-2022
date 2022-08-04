2e1ffe10a5e13eb1396361a2bbfddea2f16e994e
Con Sınov <hakanunal@devtagon.com>
2022-08-05 00:21:26 +0300

    updated

---
+++ 5-)operators/logic.py
@@ -0,0 +1,88 @@
+print(True+False+50)
+print(int(True))
+print(float(False))
+
+
+x=30
+
+sonuc=5<x<25
+sonuc=(5<x)and(x<25)
+sonuc=(5<x)or(x<25)
+sonuc=not(5<x)
+
+
+
+print(sonuc)
+
+
+#Uygulama
+
+
+number=int(input("Please enter number: "))
+
+result1=(50<number)and(number<100)
+
+if(result1):print("number is  range 50-100")
+else:print("number is not  range 50-100")
+
+result2=(0<number)and(number%2==1)
+
+if(result1):print("number is  odd and positive")
+else:print("number is not  odd and positive")
+
+
+user="test@test.com"
+password="123456"
+
+inputUser=input("please enter inputUser: ")
+inputPassword=input("please enter inputPassword: ")
+
+if(inputUser==user and inputPassword==password ):print("Login")
+else:print("Invalid")
+
+
+number1=int(input("Please enter number1: "))
+number2=int(input("Please enter number2: "))
+number3=int(input("Please enter number3: "))
+
+resultA=(number1<number2)and(number2<number3)
+resultB=(number1<number3)and(number3<number2)
+resultC=(number3<number2)and(number2<number1)
+resultD=(number3<number1)and(number1<number2)
+resultE=(number2<number1)and(number1<number3)
+resultF=(number2<number3)and(number3<number1)
+
+
+if(resultA):print("number1<number2<number3")
+elif(resultB):print("number1<number3<number2")
+elif(resultC):print("number3<number2<number1")
+elif(resultD):print("number3<number1<number2")
+elif(resultE):print("number2<number1<number3")
+elif(resultF):print("number2<number3<number1")
+
+
+userName=input("please enter username: ")
+userWeight=float(input("please enter user weight: "))
+userHeight=float(input("please enter user height: "))
+
+
+user={
+    "name":userName,
+    "weight":userWeight,
+    "height":userHeight
+}
+
+
+
+userIndex=user.get("weight")/((user.get("height")/100)**2)
+
+ındex1=(0<userIndex)and(userIndex<18.4)
+ındex2=(18.5<userIndex)and(userIndex<24.9)
+ındex3=(25<userIndex)and(userIndex<29.9)
+ındex4=(30<userIndex)and(userIndex<34.9)
+
+if(ındex1):print(f"Zayıf {userIndex}")
+elif(ındex2):print(f"Normal {userIndex}")
+elif(ındex3):print(f"Fazla Kilolu {userIndex}")
+elif(ındex4):print(f"Şişman {userIndex}")
+else:print("Invalid")
--- 5-)operators/operator.py
+++ 5-)operators/operator.py
@@ -1,31 +1,80 @@
-a,b,c=2,5,10
+# a,b,c=2,5,10
 
-inputA=int(input("enter first number: "))
-inputB=int(input("enter second number: "))
+# inputA=int(input("enter first number: "))
+# inputB=int(input("enter second number: "))
 
-result1=inputA*inputB
-result2=a+b+c
-result3=result1-result2
-
-
-print(result1)
-print(result2)
-print(result3)
-
-print(c//b)
-print(result2%3)
+# result1=inputA*inputB
+# result2=a+b+c
+# result3=result1-result2
 
 
+# print(result1)
+# print(result2)
+# print(result3)
 
-numbers=(1,5,7,10,3)
+# print(c//b)
+# print(result2%3)
 
 
-d,*e,f=numbers
 
-print(f**3)
-g=0
+# numbers=(1,5,7,10,3)
 
-for data in e:
-    g+=data
 
-print(g)
+# d,*e,f=numbers
+
+# print(f**3)
+# g=0
+
+# for data in e:
+#     g+=data
+
+# print(g)
+
+
+
+
+#Uygulama
+
+
+a=int(input("please enter a: "))
+b=int(input("please enter b: "))
+c=int(input("please enter c: "))
+
+
+if(a>b):print("a bigger than b")
+elif(a<b):print("b bigger than a")
+elif(a==b):print("a equal to b")
+
+
+if(c%2==0):print("c number is even")
+elif(c%2==1):print("c number is odd")
+
+if(c>0):print("c number is positive")
+elif(c<0):print("c number is negative")
+
+
+visit1=int(input("please enter visit1: "))
+visit2=int(input("please enter visit2: "))
+final=int(input("please enter final: "))
+
+calculate=((visit1+visit2)/2*60/100)+(final*40/100)
+
+sonuc=""
+
+if(calculate<50):sonuc="Kaldı"
+if(calculate>=50):sonuc="Geçti"
+
+
+print(f"your result: {sonuc,calculate}")
+
+
+user="test@test.com"
+password="123456"
+
+inputUser=input("please enter inputUser: ")
+inputPassword=input("please enter inputPassword: ")
+
+if(inputUser==user and inputPassword==password ):print("Login")
+else:print("Invalid")
+
+

a00e9c49a9f51b0ee579a6a0d9cabd3b3924b7e0
Con Sınov <hakanunal@devtagon.com>
2022-08-14 01:47:38 +0300

    updated

--- 13-)module/demo/app.py
+++ 13-)module/demo/app.py
@@ -2,4 +2,30 @@ from methods import *
 
 
 
-print(getProducts())
+while True:
+    print("1-) Add Product")
+    print("2-) Get Product")
+    print("3-) Get Products")
+    print("4-) Remove Product")
+
+    choice= input("Choice operations: 1/2/3/4: ")
+    match (choice):
+        case "1":
+            name=input("Please enter product name:")
+            price=input("Please enter product price:")
+            addProduct(name,price)
+        case "2":
+            id=int(input("Please enter product id:"))
+            print(getProduct(id))
+        case "3":
+            print(getProducts())
+        case "4":
+            id=int(input("Please enter product id:"))
+            removeProduct(id)
+        case default:print("Invalid Choice")
+
+
+
+
+
+

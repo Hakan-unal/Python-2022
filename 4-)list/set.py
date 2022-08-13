ff705c880577c3948d5a432bc60e32dfa3fc87ca
Con Sınov <hakanunal@devtagon.com>
2022-08-13 18:52:44 +0300

    updated

--- 4-)list/set.py
+++ 4-)list/set.py
@@ -1,4 +1,3 @@
-from cgi import test
 
 
 fruits={
---
+++ 6-)if-else/switch.py
@@ -0,0 +1,14 @@
+ 
+
+choice=input("please enter choice (A/B/C): ")
+
+
+match choice:
+    case "A":
+        print("A")
+    case "B":
+        print("B")
+    case "C":
+        print("C")
+    case default:
+        print("Not valid")
